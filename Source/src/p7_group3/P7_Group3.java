ff9bd05f7c29c641a3ddaa0f0450c25e39339bc9
Mette <mettehyllested3@gmail.com>
2016-11-20 19:16:38 +0100

    Changed the word World in the text "Say 'Hello World'" to small letters: world.

--- Source/src/p7_group3/P7_Group3.java
+++ Source/src/p7_group3/P7_Group3.java
@@ -22,7 +22,7 @@ public class P7_Group3 extends Application {
     @Override
     public void start(Stage primaryStage) {
         Button btn = new Button();
-        btn.setText("Say 'Hello World'");
+        btn.setText("Say 'Hello world'");
         btn.setOnAction(new EventHandler<ActionEvent>() {
             
             @Override
