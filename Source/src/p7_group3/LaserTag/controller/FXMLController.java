81f7814396e2852b68167d5933e775f192dc95e5
Mette <mettehyllested3@gmail.com>
2016-11-26 23:16:38 +0100

    Create interaction between main page, login page and maintenance page.

--- Source/src/p7_group3/LaserTag/controller/FXMLController.java
+++ Source/src/p7_group3/LaserTag/controller/FXMLController.java
@@ -1,5 +1,6 @@
 package p7_group3.LaserTag.controller;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -7,11 +8,16 @@ import p7_group3.LaserTag.model.Table;
 
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.stage.Stage;
 
 
 /**
@@ -50,5 +56,13 @@ public class FXMLController implements Initializable {
         tableID.setItems(data);
     
     }   
+    
+    public void openMaintenanceView(ActionEvent event) throws IOException {
+        Stage stage = new Stage();
+                        Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("p7_group3/LaserTag/view/LoginView.fxml"));
+                        Scene scene = new Scene(root);
+                        stage.setScene(scene);
+                        stage.show();
+    }
     
 }
