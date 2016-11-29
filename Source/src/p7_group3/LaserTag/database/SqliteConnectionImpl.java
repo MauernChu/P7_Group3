1867a6709cebdecf877cc1269f643cc46ad48c82
Mette <mettehyllested3@gmail.com>
2016-11-29 19:42:28 +0100

    Initial work for Sqlite

---
+++ Source/src/p7_group3/LaserTag/database/SqliteConnectionImpl.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package p7_group3.LaserTag.database;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author Mette
+ */
+public class SqliteConnectionImpl implements DbConnection {
+
+    public Connection createConnection() {
+        try{
+            Class.forName("org.sqlite.JDBC");
+            Connection conn = DriverManager.getConnection("jdbc:sqlite:\\.\\.\\Database\\LaserTagDb.db");
+            return conn;
+        }catch (ClassNotFoundException | SQLException e){
+            return null;
+        }
+    }
+}
