a3ef8a71ad9e146e5ed86a0fef2130b2ba866569
KLS\karatinka <sakacova.katarina@gmail.com>
2016-12-09 12:58:45 +0100

    created pop up for Add equipment, changed

--- Source/src/p7_group3/LaserTag/controller/AddEquipmentViewController.java
+++ Source/src/p7_group3/LaserTag/controller/AddEquipmentViewController.java
@@ -7,7 +7,9 @@ package p7_group3.LaserTag.controller;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.stage.Stage;
 
 /**
  * FXML Controller class
@@ -26,7 +28,27 @@ public class AddEquipmentViewController implements Initializable {
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
-    } 
+    }
+    
+     //when pushing Cancle button, the pop up window closes
+    @FXML
+    private javafx.scene.control.Button cancleAddEquipmentButton;
+    
+    @FXML
+    public void cancleAddEquipmentButtonAction() {
+        Stage stage = (Stage) cancleAddEquipmentButton.getScene().getWindow();
+        stage.close();
+    }
+    
+     //when pushing Submit button new equipment will be added to database
+    @FXML
+    private javafx.scene.control.Button submitAddEquipmentButton;
+    
+    @FXML
+    public void submitAddEquipmentButtonAction() {
+        Stage stage = (Stage) submitAddEquipmentButton.getScene().getWindow();
+        stage.close();
+    }
     
     public void setChargingViewController(ChargingViewController tableViewController) {
         this.chargingViewController = tableViewController;
